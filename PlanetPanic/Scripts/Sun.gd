24855e3dfe0af3624b0ab3d293740d619bc0d6d1
Wentong-Wu <80329793+Wentong-Wu@users.noreply.github.com>
2021-03-29 22:11:22 +0100

    Sprites
    Added some sprites. Sun plays animation when eat and goes back to default when finished.

--- PlanetPanic/Scripts/Sun.gd
+++ PlanetPanic/Scripts/Sun.gd
@@ -4,11 +4,19 @@ signal asteroid_hit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	$AnimatedSprite.play("default")
 	pass # Replace with function body.
+
+
 
 func _on_Sun_Area_area_entered(_area: Area2D) -> void:
 	if "Asteroid" in _area.get_parent().name:
 		_area.get_parent().queue_free()
+		$AnimatedSprite.play("Sun_eat")
 		emit_signal("asteroid_hit")
+	pass # Replace with function body.
+
+
+func _on_AnimatedSprite_animation_finished():
+	$AnimatedSprite.play("default")
 	pass # Replace with function body.
