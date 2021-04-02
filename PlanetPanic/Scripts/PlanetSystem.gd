44026e0bdec2ca39e3ea15381cbae24f0423a798
ScrappyMan25 <53944571+ScrappyMan25@users.noreply.github.com>
2021-04-02 13:21:41 +0100

    Orbit update
    Update to dotted line orbit

--- PlanetPanic/Scripts/PlanetSystem.gd
+++ PlanetPanic/Scripts/PlanetSystem.gd
@@ -196,8 +196,8 @@ func draw_circle_arc(center, radius, angle_from, angle_to, color):
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
 
 	for index_point in range(nb_points):
-		draw_line(points_arc[index_point], points_arc[index_point + 1], color)
-#		draw_multiline(points_arc, color, 2.0)
+#		draw_line(points_arc[index_point], points_arc[index_point + 1], color)
+		draw_multiline(points_arc, color, 2.0)
 
 #Signals
 
